7dcf59469232e5181a867bea3a92a5a2f772c1c0
Volodymyr Shyrochuk <vov4uks@gmail.com>
2024-07-26 18:08:22 +0300

    Updated deploy screen

--- deploy_web.sh
+++ deploy_web.sh
@@ -3,7 +3,7 @@ git clone --branch main git@github.com:Mc231/Mc231.github.io.git ../gh-pages
 rm -rf ../gh-pages
 rsync -av --delete build/web/ ../gh-pages/
 # shellcheck disable=SC2164
-cd ../gh-pages
+pwd
 # shellcheck disable=SC2035
 git add *
 git commit -m "Deploy to GitHub Pages"
