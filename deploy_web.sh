c86ec75a8144b9acfdb76b74b2a636e11ec246ae
Volodymyr Shyrochuk <vov4uks@gmail.com>
2024-07-26 17:58:43 +0300

    Updated deploy script

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,7 +1,9 @@
 flutter build web --release
 git clone --branch main git@github.com:Mc231/Mc231.github.io.git ../gh-pages
 rsync -av --delete build/web/ ../gh-pages/
-../cd gh-pages
+# shellcheck disable=SC2164
+cd ../gh-pages
+# shellcheck disable=SC2035
 git add *
 git commit -m "Deploy to GitHub Pages"
 git push
