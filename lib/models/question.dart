3db224eddda88d35f655dddf90bcddf542213c43
Volodymyr Shyrochuk <vov4uks@gmail.com>
2024-08-03 20:54:39 +0300

    Added integration tests

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -4,7 +4,6 @@ class Question<T> {
   final T answer;
   final List<T> options;
 
-  // TODO: - Add check options can not be empty
   Question(this.answer, this.options);
 
   Question.fromRandomResult(RandomPickResult<T> result)
